b5de06d74db0c63cad2d941f84b7a61316fb6341
xXxRosxXx <suttaweelukuan@gmail.com>
2016-11-15 15:53:42 +0700

    580610683

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,15 @@
 public class Teacher extends User {
+
+    public Teacher(){}
+    public Teacher(String n, String p, int t) {
+        super(n, p,USER_TEACHER);
+    }
+
+
+
+
+
+
+
+
 }
--- src/User.java
+++ src/User.java
@@ -1,26 +1,41 @@
 public class User implements IUser {
+    public String name,password;
+    public int type;
+    public User(){}
+    public User(String n,String p,int t){
+        name=(n.equals(""))?"Anonymous":n;
+        password=setPassword(p);
+        type=t;
+    }
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     @Override
-    public String setName(String name) {
-        return null;
+    public String setName(String n) {name=(n.equals(""))?"Anonymous":n;
+        return name;
     }
 
     @Override
-    public String setPassword(String password) {
-        return null;
+    public String setPassword(String p) {
+        if(p.length()>=8){
+            password=p;
+        return p;
+    }
+    p="mypassword";
+    System.out.println("Password must be 8 characters or longer ");
+        System.out.println("your password is automatically set to \"mypassword\"");
+    return "mypassword";
     }
 
     @Override
-    public boolean isPasswordCorrect(String password) {
-        return false;
+    public boolean isPasswordCorrect(String p) {
+        return p.equals(this.password);
     }
 
     @Override
     public int getType() {
-        return 0;
+        return type;
     }
 }
