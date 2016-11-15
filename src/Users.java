54b9f932bb1167cde52cae1eda52f9d8e675cf39
thanaphom <pamdhumawong@gmail.com>
2016-11-15 14:30:59 +0700

    Users

--- src/Users.java
+++ src/Users.java
@@ -7,28 +7,34 @@ public class Users {
     // Create new user with type, name and password
     // user created with this method should be automatically added to userList;
     public IUser create(int type, String name, String password) {
+        User user = new User();
+        user.setName(name);
+        user.type = type;
+        user.setPassword(password) ;
         return null;
     }
 
     // Add new user to repository
     public void add(IUser user) {
+    userList.add(user);
 
     }
 
     // Delete user from repository
     // Throw  RuntimeException if the user is not in the list
     public void remove(IUser user) throws RuntimeException {
+        userList.remove(user);
 
     }
 
     // Return true if the user is in the list
     public boolean exists(IUser user) {
-        return false;
+        return userList.contains(user);
     }
 
     // Return number of user in the list
     public int count() {
-        return 0;
+        return userList.size();
     }
 
     // Return number of user in the list, according to type
